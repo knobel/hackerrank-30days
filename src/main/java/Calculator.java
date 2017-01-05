06c809a825752ae6a1d58d9f17389d432bb68921
michal <WUwuzela1>
2017-01-05 08:48:29 +0100

    Calculator - day 17

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,44 @@
+
+
+
+
+import java.util.Scanner;
+
+/**
+ * Created by michal on 05.01.17.
+ */
+class Calculator {
+    int power(int base, int exponent) {
+        if (base < 0 || exponent < 0)
+            throw new IllegalArgumentException("n and p should be non-negative");
+        if (exponent == 0)
+            return 1;
+        return base*power(base, exponent-1);
+    }
+}
+
+class Solution{
+
+    public static void main(String []argh)
+    {
+        Scanner in = new Scanner(System.in);
+        int T=in.nextInt();
+        while(T-->0)
+        {
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try
+            {
+                int ans=myCalculator.power(n,p);
+                System.out.println(ans);
+
+            }
+            catch(Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+}
